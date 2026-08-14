49620239e86b47bc575035b4b2d3b4d922de49d9
Pedro Lucas Rodrigues <pedrolr2060@gmail.com>
2026-08-13 21:47:27 -0300

    Atualização DDL

--- DDL - REVISAO DB.sql
+++ DDL - REVISAO DB.sql
@@ -2,10 +2,10 @@ create database if not exists db_begin
 default character set utf8
 default collate utf8_general_ci;
 
-use db_general;
+use db_begin;
 
 create table if not exists cliente (
-	pk_cpf char(11) not null primary key,
+	pk_cpf char(11) primary key not null,
     nome_cliente varchar(75) not null,
     dt_nacimento date not null,
     num_celular char(11) not null,
@@ -13,22 +13,21 @@ create table if not exists cliente (
 )
 default character set utf8;
 
-create table if not exists usuarios (
-	pk_usuario char(12) not null primary key,
-    fk_email varchar(75) not null,
+create table if not exists usuario (
+	pk_id_usuario int auto_increment primary key,
+	fk_cpf char(11) not null,
+	usuario char(12) not null,
     senha char(15) not null,
     
-    foreign key (fk_emai) references cliente(email)
+    foreign key (fk_cpf) references cliente(pk_cpf)
 )
 default character set utf8;
 
 create table if not exists cnh (
 	pk_cnh char(9) primary key not null,
     fk_cpf char(11) not null,
-    fk_nome_cliente varchar(75) not null,
-    
-    foreign key (fk_cpf) references cliente(pk_cpf),
-    foreign key (fk_nome_cliente) references cliente(_nome_cliente)
+
+    foreign key (fk_cpf) references cliente(pk_cpf)
 )
 default character set utf8;
 
@@ -40,10 +39,30 @@ create table if not exists carro (
     placa  char(7) not null,
     cor varchar(25) not null,
     categoria varchar(2) not null, -- sera definido por A ou B ou AB...
-    pt_motor char(3) not null,
+    pt_motor char(3) not null
 ) 
 default character set utf8;
 
+create table if not exists reserva (
+	pk_id_reserva int auto_increment primary key,
+    fk_cpf char(11) not null,
+    dt_retirada date not null,
+    hr_retirada time not null,
+    dt_devolucao date not null,
+    hr_devolucao time not null,
+    tp_retirada char(1) not null,
+    tp_devolucao char(1) not null,
+    valor decimal(7,2),
+    
+	foreign key (fk_cpf) references cliente(pk_cpf)
+)
+default character set utf8;
 
-
-
+create table if not exists funcionarios (
+	pk_cpf char(11) not null primary key,
+    nome_cliente varchar(75) not null,
+    dt_nacimento date not null,
+    num_celular char(11) not null,
+    email varchar(75) not null
+)
+default character set utf8;
